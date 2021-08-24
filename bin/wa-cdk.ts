6eea34e72ccc98e8990468db7f4c7ed095105946
muhammadtahir <mstahir77@gmail.com>
2021-08-23 22:27:50 -0700

    Adding tags to the stack resources

--- bin/wa-cdk.ts
+++ bin/wa-cdk.ts
@@ -3,6 +3,21 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { WaCdkStack } from '../lib/wa-cdk-stack';
 import {WABucket} from "../lib/constructs/wa-s3/WABucket";
+import {CfnParameter, TagManager, Tags, TagType} from "@aws-cdk/core";
 
 const app = new cdk.App();
-new WaCdkStack(app, 'WaCdkStack', {});
+
+const stack = new WaCdkStack(app, 'WaCdkStack', {});
+
+const project = new CfnParameter(stack, 'Project', {
+    type: 'String',
+    default: "Sample",
+});
+
+const environment = new CfnParameter(stack, 'Environment', {
+    type: 'String',
+    default: "Development",
+});
+
+Tags.of(stack).add(project.logicalId, project.valueAsString)
+Tags.of(stack).add(environment.logicalId, environment.valueAsString)
--- lib/wa-cdk-stack.ts
+++ lib/wa-cdk-stack.ts
@@ -4,7 +4,6 @@ import {WABucket} from "./constructs/wa-s3/WABucket";
 export class WaCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     new WABucket(this, "MyBucket", {logBucketName: "arn:aws:s3:::cl-s3-logs"})
   }
 }
